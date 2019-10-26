12f4516a4fecff6f6cb24b01aa9951379d16f63a
DerEros <dereros@noreply.users.github.com>
2019-10-26 18:08:14 +0200

    Implement proper candle flicker for NormalAnimation
    Will need to refactor in order to have it for SpookyAnimation as well.

--- NormalAnimation.cpp
+++ NormalAnimation.cpp
@@ -2,14 +2,45 @@
 
 #include <FastLED.h>
 
+
+DEFINE_GRADIENT_PALETTE(regularPaletteDef) {
+    0,   0,   0,   0,
+    138, 255, 0,   0,
+    178,  42,  17, 0,
+    255, 228, 151, 6
+};
+
+CRGBPalette32 _palette = regularPaletteDef;
+
 void NormalAnimation::handleFrameSwitch() {
-    if (on) {
-        fill_solid(_pixels, _numPixels, CRGB::Red);
-    } else {
-        fill_solid(_pixels, _numPixels, CRGB::Black);
+    if (_paletteIndicies == NULL) {
+        initIndices();
     }
 
-    on = !on;
+    flicker();
+    renderPixels();
 
     FastLED.show();
+}
+
+void NormalAnimation::initIndices() {
+    _paletteIndicies = new uint8_t[_numPixels];
+    for (unsigned int i = 0; i < _numPixels; i++) {
+        _paletteIndicies[i] = random8();
+    }
+}
+
+void NormalAnimation::flicker() {
+    for (unsigned int i = 0; i < _numPixels; i++) {
+        int8_t change = random8(_flickerRate);
+        change -= _flickerRate / 2;
+
+        _paletteIndicies[i] += change;
+    }
+}
+
+void NormalAnimation::renderPixels() {
+    for (int i = 0; i < _numPixels; i++) {
+        _pixels[i] = ColorFromPalette(_palette, _paletteIndicies[i]);
+    }
 }
--- NormalAnimation.h
+++ NormalAnimation.h
@@ -8,15 +8,22 @@
 class NormalAnimation : public AbstractFrameAnimation {
     public:
         NormalAnimation(CRGB pixels[], unsigned int numPixels, Fps fps) :
-            AbstractFrameAnimation(fps), _pixels(pixels), _numPixels(numPixels)
+            AbstractFrameAnimation(fps), 
+            _pixels(pixels), 
+            _numPixels(numPixels),
+            _paletteIndicies(NULL),
+            _flickerRate(20)
         {}
 
     private:
         CRGB *_pixels;
         unsigned int _numPixels;
-
-        bool on = true;
+        uint8_t *_paletteIndicies;
+        int8_t _flickerRate;
 
         virtual void handleFrameSwitch();
+        void initIndices();
+        void flicker();
+        void renderPixels();
 };
 #endif
